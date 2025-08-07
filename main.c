5da2a10a6b7972f119339a637eb36a1ec7976876
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-07 11:45:01 +0800

    create socket

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define DEFAULT_IRC_PORT "6667"
 
@@ -19,6 +20,17 @@ int main(void)
 		return 1;
 	}
 
+	int sockfd = socket(serv_info->ai_family, serv_info->ai_socktype, serv_info->ai_protocol);
+	if (sockfd < 0) {
+		perror("creating socket error");
+		goto defer;
+	}
+
 	freeaddrinfo(serv_info);
+	close(sockfd);
 	return 0;
+
+defer:
+	freeaddrinfo(serv_info);
+	return 1;
 }
